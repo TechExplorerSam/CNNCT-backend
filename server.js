4c0845f92b933c2ad5d71a9f8f2cbcd762f1fcc1
Sampath23 <140730394+TechExplorerSam@users.noreply.github.com>
2025-03-31 21:04:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/user/bookings',BookingRoutes)
 app.use('/user/timeslotavailabilty',UserAvailabilityTimeSlotRoutes)
 
 
-mongoose.connect('mongodb+srv://mathasampath2022:VgfKAc8Le0rwgsTO@cnnct2.ftoqbnh.mongodb.net/?retryWrites=true&w=majority&appName=CNNCT2')
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log('Failed to connect to MongoDB', err));
 app.listen(port,()=>{
